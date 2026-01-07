green-coding-solutions/nextcloud-server

fix: drop set up partial mounts on setupForUser
Signed-off-by: Salvatore Martire <4652631+salmart-dev@users.noreply.github.com>

lib/private/Files/SetupManager.php
@@ -236,6 +236,8 @@ class SetupManager {
 
 		$this->eventLogger->start('fs:setup:user:full', 'Setup full filesystem for user');
 
+		$this->dropPartialMountsForUser($user);
+
 		$this->setupUserMountProviders[$user->getUID()] ??= [];
 		$previouslySetupProviders = $this->setupUserMountProviders[$user->getUID()];
 
@@ -656,6 +658,7 @@ class SetupManager {
 			$this->eventLogger->end('fs:setup:user:providers');
 			return;
 		} else {
+			$this->dropPartialMountsForUser($user, $providers);
 			$this->setupUserMountProviders[$user->getUID()] = array_merge($setupProviders, $providers);
 			$mounts = $this->mountProviderCollection->getUserMountsForProviderClasses($user, $providers);
 		}
@@ -737,6 +740,41 @@ class SetupManager {
 	private function registerMounts(IUser $user, array $mounts, ?array $mountProviderClasses = null): void {
 		if ($this->lockdownManager->canAccessFilesystem()) {
 			$this->userMountCache->registerMounts($user, $mounts, $mountProviderClasses);
+		}
+	}
+
+	/**
+	 * Drops partially set-up mounts for the given user
+	 * @param class-string<IMountProvider>[] $providers
+	 */
+	public function dropPartialMountsForUser(IUser $user, array $providers = []): void {
+		// mounts are cached by mount-point
+		$mounts = $this->mountManager->getAll();
+		$partialMounts = array_filter($this->setupMountProviderPaths,
+			static function (string $mountPoint) use (
+				$providers,
+				$user,
+				$mounts
+			) {
+				$userMount = str_starts_with($mountPoint, '/' . $user->getUID() . '/files');
+
+				if (!$userMount) {
+					return false;
+				}
+
+				$mountProvider = ($mounts[$mountPoint] ?? null)?->getMountProvider();
+
+				return empty($providers)
+					|| \in_array($mountProvider, $providers, true);
+			},
+			ARRAY_FILTER_USE_KEY);
+
+		if (!empty($partialMounts)) {
+			// remove partially set up mounts
+			foreach ($partialMounts as $mountPoint => $_mount) {
+				$this->mountManager->removeMount($mountPoint);
+				unset($this->setupMountProviderPaths[$mountPoint]);
+			}
 		}
 	}
 }

February30th/server

lib/private/Files/SetupManager.php
@@ -236,6 +236,8 @@ class SetupManager {
 
 		$this->eventLogger->start('fs:setup:user:full', 'Setup full filesystem for user');
 
+		$this->dropPartialMountsForUser($user);
+
 		$this->setupUserMountProviders[$user->getUID()] ??= [];
 		$previouslySetupProviders = $this->setupUserMountProviders[$user->getUID()];
 
@@ -658,6 +660,7 @@ class SetupManager {
 			$this->eventLogger->end('fs:setup:user:providers');
 			return;
 		} else {
+			$this->dropPartialMountsForUser($user, $providers);
 			$this->setupUserMountProviders[$user->getUID()] = array_merge($setupProviders, $providers);
 			$mounts = $this->mountProviderCollection->getUserMountsForProviderClasses($user, $providers);
 		}
@@ -739,6 +742,41 @@ class SetupManager {
 	private function registerMounts(IUser $user, array $mounts, ?array $mountProviderClasses = null): void {
 		if ($this->lockdownManager->canAccessFilesystem()) {
 			$this->userMountCache->registerMounts($user, $mounts, $mountProviderClasses);
+		}
+	}
+
+	/**
+	 * Drops partially set-up mounts for the given user
+	 * @param class-string<IMountProvider>[] $providers
+	 */
+	public function dropPartialMountsForUser(IUser $user, array $providers = []): void {
+		// mounts are cached by mount-point
+		$mounts = $this->mountManager->getAll();
+		$partialMounts = array_filter($this->setupMountProviderPaths,
+			static function (string $mountPoint) use (
+				$providers,
+				$user,
+				$mounts
+			) {
+				$isUserMount = str_starts_with($mountPoint, '/' . $user->getUID() . '/files');
+
+				if (!$isUserMount) {
+					return false;
+				}
+
+				$mountProvider = ($mounts[$mountPoint] ?? null)?->getMountProvider();
+
+				return empty($providers)
+					|| \in_array($mountProvider, $providers, true);
+			},
+			ARRAY_FILTER_USE_KEY);
+
+		if (!empty($partialMounts)) {
+			// remove partially set up mounts
+			foreach ($partialMounts as $mountPoint => $_mount) {
+				$this->mountManager->removeMount($mountPoint);
+				unset($this->setupMountProviderPaths[$mountPoint]);
+			}
 		}
 	}
 }